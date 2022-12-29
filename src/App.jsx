b272dd9443422470afe240a2d148489c273ab0b3
Gabriel Spada <gabrielspadadesouza@gmail.com>
2022-12-29 15:40:40 -0300

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header, Footer } from './components';
+import { Home, Contact } from './pages';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
