1c3d4662d6c24d05a6856d0e03fba415fbf8a7a4
Gabriel Spada <gabrielspadadesouza@gmail.com>
2023-01-03 17:13:23 -0300

    Add product done

--- src/components/Admin/AddProduct/AddProduct.jsx
+++ src/components/Admin/AddProduct/AddProduct.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { toast } from 'react-toastify';
+import { db, storage } from '../../../firebase/config';
 import Card from '../../Card/Card';
 import styles from './AddProduct.module.scss';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
 
 const categories = [
   { id: 1, name: 'Router' },
@@ -10,26 +14,68 @@ const categories = [
   { id: 5, name: 'NAS' },
 ];
 
+const initialState = {
+  name: '',
+  imageURL: '',
+  price: '',
+  category: '',
+  brand: '',
+  desc: '',
+};
+
 const AddProduct = () => {
-  const [product, setProduct] = useState({
-    name: '',
-    imageURL: '',
-    price: '',
-    category: '',
-    brand: '',
-    desc: '',
-  });
+  const [product, setProduct] = useState({ ...initialState });
+  const [uploadProgress, setUploadProgress] = useState(0);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setProduct({ ...product, [name]: value });
   };
 
-  const handleImageChange = (e) => {};
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const storageRef = ref(storage, `images_gp-shop/${Date.now()}${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+      },
+      (error) => {
+        toast.error(`[${error.code}] ${error.message}`, { theme: 'colored' });
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setProduct({ ...product, imageURL: downloadURL });
+          toast.success('Image uploaded successfully', { theme: 'colored' });
+        });
+      }
+    );
+  };
 
   const addProduct = (e) => {
     e.preventDefault();
-    console.log(product);
+    toast.loading('Processing your request...');
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const docRef = addDoc(collection(db, 'products'), {
+        name: product.name,
+        imageURL: product.imageURL,
+        price: Number(product.price),
+        categories: product.category,
+        brand: product.brand,
+        desc: product.desc,
+        createdAt: Timestamp.now().toDate(),
+      });
+      setProduct({ ...initialState });
+      setUploadProgress(0);
+      toast.dismiss();
+      toast.success('Product added successfully', { theme: 'colored' });
+    } catch (err) {
+      toast.dismiss();
+      toast.error(`${err}`, { theme: 'colored' });
+    }
   };
 
   return (
@@ -47,13 +93,15 @@ const AddProduct = () => {
             onChange={(e) => handleInputChange(e)}
           />
           <label>Image:</label>
-          <div className={styles.progress}>
-            <div className={styles['progress-bar']} style={{ width: '50%' }}>
-              Uploading 50%
+          {uploadProgress === 0 ? null : (
+            <div className={styles.progress}>
+              <div className={styles['progress-bar']} style={{ width: `${uploadProgress}%` }}>
+                {uploadProgress < 100 ? `Uploading ${uploadProgress}%` : 'Upload completed!'}
+              </div>
             </div>
-          </div>
+          )}
           <input type='file' placeholder='Product Image' accept='image/*' name='image' onChange={(e) => handleImageChange(e)} />
-          <input type='text' name='imageURL' value={product.imageURL} disabled />
+          {product.imageURL === '' ? null : <input type='text' name='imageURL' value={product.imageURL} disabled />}
           <label>Price:</label>
           <input
             type='number'
